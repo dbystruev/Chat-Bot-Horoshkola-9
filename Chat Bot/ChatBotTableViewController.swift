9789bb755f63fb8453daff1aa2ceaf9c0b222d4a
Denis Bystruev <dbystruev@me.com>
2019-03-18 10:41:42 +0300

    Added answers array and alert controller

--- Chat Bot/ChatBotTableViewController.swift
+++ Chat Bot/ChatBotTableViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ChatBotTableViewController: UITableViewController {
     
+    var answers: [String] = [
+        "9:00 Консультация по ОГЭ по русскому языку",
+        "10:00 Информатика",
+        "10:50 Английский язык",
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let alert = UIAlertController(title: "Введите вопрос", message: nil, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return answers.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
         
-        cell.textLabel?.text = "Row \(indexPath.row)"
+        let index = indexPath.row
+        cell.textLabel?.text = answers[index]
         
         return cell
     }
